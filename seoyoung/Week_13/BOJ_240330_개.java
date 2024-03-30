6538636a59af7631b8f0c480ecd179a45cad4acd
westzero77 <westzero713@gmail.com>
2024-03-30 17:39:59 +0900

    [BOJ] 개 / 1단계 / 5분 / https://www.acmicpc.net/problem/10172

---
+++ seoyoung/Week_13/BOJ_240330_개.java
@@ -0,0 +1,16 @@
+package seoyoung.Week_13;
+
+public class BOJ_240330_개 {
+  //|\_/|
+  //|q p|   /}
+  //( 0 )"""\
+  //|"^"`    |
+  //||_/=\\__|
+  public static void main(String[] args) {
+    System.out.println("|\\_/|");
+    System.out.println("|q p|   /}");
+    System.out.println("( 0 )\"\"\"\\");
+    System.out.println("|\"^\"`    |");
+    System.out.println("||_/=\\\\__|");
+  }
+}
