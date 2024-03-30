fc02577f85c39df0eaab7596117049249dd391a5
westzero77 <westzero713@gmail.com>
2024-03-30 17:33:52 +0900

    [BOJ] A/B / 1단계 / 1분 / https://www.acmicpc.net/problem/1008

---
+++ seoyoung/Week_13/BOJ_240330_a_division_b.java
@@ -0,0 +1,12 @@
+package seoyoung.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_a_division_b {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double a = scanner.nextInt();
+    double b = scanner.nextInt();
+    System.out.println(a/b);
+  }
+}
