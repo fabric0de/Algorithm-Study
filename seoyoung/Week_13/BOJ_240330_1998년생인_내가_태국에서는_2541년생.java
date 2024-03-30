0b8a67f444412698af05a8b08ef283e118970793
westzero77 <westzero713@gmail.com>
2024-03-30 17:36:26 +0900

    [BOJ] 1998년생인 내가 태국에서는 2541년생?! / 1단계 / 5분 / https://www.acmicpc.net/problem/18108

---
+++ seoyoung/Week_13/BOJ_240330_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,13 @@
+package seoyoung.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_1998년생인_내가_태국에서는_2541년생 {
+  public static void main(String[] args) {
+    int BUDDHA_YEAR = 543;
+    Scanner scanner = new Scanner(System.in);
+    int scan_year = scanner.nextInt();
+
+    System.out.println(scan_year - BUDDHA_YEAR);
+  }
+}
