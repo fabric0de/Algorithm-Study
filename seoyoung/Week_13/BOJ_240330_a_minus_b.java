cd9df3776a558a06b523ce7b2d053e25bab7da02
westzero77 <westzero713@gmail.com>
2024-03-30 17:32:50 +0900

    [BOJ] A-B / 1단계 / 1분 / https://www.acmicpc.net/problem/1001

---
+++ seoyoung/Week_13/BOJ_240330_a_minus_b.java
@@ -0,0 +1,12 @@
+package seoyoung.Week_13;
+
+import java.util.Scanner;
+
+public class BOJ_240330_a_minus_b {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a-b);
+  }
+}
