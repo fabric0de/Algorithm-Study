66f36eb029de0dd660b0a1f7b23c00750abc867f
westzero77 <westzero713@gmail.com>
2024-04-06 18:22:03 +0900

    [BOJ] 별 찍기 - 2 / 브론즈5 / 1분 / https://www.acmicpc.net/problem/2439

---
+++ seoyoung/Week_14/BOJ_240406_별찍기_2.java
@@ -0,0 +1,20 @@
+package seoyoung.Week_14;
+
+import java.util.Scanner;
+
+public class BOJ_240406_별찍기_2 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int starCount = scanner.nextInt();
+
+    for(int i=1; i<=starCount; i++) {
+      for(int j=0; j<(starCount-i); j++) {
+        System.out.print(" ");
+      }
+      for(int j=0; j<i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
